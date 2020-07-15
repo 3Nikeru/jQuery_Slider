e1ac86d1e5461d33036522ddf18d3225a283f0da
Student <student@lan.itstep.kiev.ua>
2020-07-15 21:14:57 +0300

    first commit

---
+++ lib.js
@@ -0,0 +1,45 @@
+class Slider{
+	constructor(id, width, height, array){ 
+      this._box = $(`#${id}`);
+      this._width = width;
+      this._height = height;
+      this._array = array;
+      this._current = 0;
+	}
+
+	displaySlider(){
+		this._box.css('width', this._width);
+		this._box.css('height', this._height);
+		this._box.show(1000);
+		this.loadImage(0, this._array[0]);
+	}
+
+	loadImage(id, path){
+        let item = document.createElement('img'); // <img class="picture">
+        $(item).addClass('picture').attr({'id': id, 'src': path}) // <img ... id="0" src="">
+         .appendTo(this._box).fadeIn(1000); // <img ... id="0" src="">
+	}
+
+	loadImages(){
+
+	}
+
+	rotateArrows(){
+
+    }
+
+    slideLeft(){
+
+    }
+
+    slideRight(){
+
+    }
+
+    activateEvents(){
+	this.rotateArrows();
+	this.slideRight();
+	this.slideLeft();
+    }
+}
+
---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+// 1 -> Создаём массив пуей к картинкам
+  const collection = [];
+  for(let i = 1; i <= 12; i++){
+    collection.push(`img/collection-1/p${i}.jpg`);
+  }
+  console.log(collection);
+
+  // 2 -> Создаём экземпляр слайдера
+  const slider = new Slider('slider_id', '1000px', '500px', collection);
+
+  slider.displaySlider();
+
+});
